61e3de16b023c1dc412a1dc5de27ff1847a05266
Katya Sheps <katya@sheps.ru>
2019-06-02 21:40:27 +0300

    Add NotesActivity.java and activity_notes.xml

--- app/src/main/java/ru/sheps/android/homework222/MainActivity.java
+++ app/src/main/java/ru/sheps/android/homework222/MainActivity.java
@@ -3,6 +3,10 @@ package ru.sheps.android.homework222;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+import com.netodologia.lessons.R;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,9 +18,22 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
     }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        int id = item.getItemId();
+
+        if (id == R.id.action_open_notes) {
+            Toast.makeText(MainActivity.this, "Отркыть записную книжку", Toast.LENGTH_LONG).show();
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
 
 }
